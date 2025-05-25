4345d6e853f6e13bc85edb3fc5180d55ba48366e
Mr-Kaos <65438130+mr-kaos@users.noreply.github.com>
2025-05-25 20:11:50 +1000

    Added license Added footer with some links

---
+++ site/private_core/templates/footer.php
@@ -0,0 +1,11 @@
+<footer>
+	<div>
+		<span>Contribute to this project <a href="https://github.com/Mr-Kaos/The-Rip-DB" target="_blank">on GitHub</a>!</span>
+	</div>
+	<div>
+		<span>Made with love for the ripping community by <a href="https://github.com/Mr-Kaos" target="_blank">Mr Kaos</a></span>
+	</div>
+	<div>
+		<span style="filter:opacity(25%);padding:0px 40px;display:block">idk what to put here yet... probably a manual or guide</span>
+	</div>
+</footer>
