c63c1a38d78d55486a0b31fa3ee0d0a44d3dd348
Mr-Kaos <65438130+mr-kaos@users.noreply.github.com>
2025-07-13 16:16:10 +1000

    Updated deployment

--- sql/deploy.php
+++ sql/deploy.php
@@ -2,128 +2,137 @@
 
 /**
  * This is a simple script that should be run in the command line to deploy the database.
- * This is written in PHP to reduce the need for additional dependencies to deploy the app, and so I don't need to write a unix bash and windows batch script to do the same thing...
+ * This is written in PHP to reduce the need for additional dependencies to deploy the app, and so I don't need to write a unix bash and windows batch script that do the same thing...
  * 
- * Set up the database connection below to deploy:
+ * Please make sure to copy the db-template.php file in 'site/private_core/config/' as 'db.php' and set your database connection details in there.
  */
 
-// ---------------
-// DATABASE CONFIG
-// ---------------
+if (!file_exists('../site/private_core/config/db.php')) {
+	echo "Please set up your database connection!\nSee 'site/private_core/config/db-template.php' for a connection template.\n";
+} else {
+	require_once('../site/private_core/config/db.php');
 
-const DB_HOST = 'localhost';
-const DB_USER = 'root';
-const DB_PASS = '';
+	// -------------------
+	// DEPLOYMENT COMMENCE
+	// -------------------
 
-// -------------------
-// DEPLOYMENT COMMENCE
-// -------------------
+	$pdo = new PDO('mysql:host=' . constant('SQL_HOST') . ';charset=UTF8mb4', constant('SQL_USER'), constant('SQL_PASS'));
 
-$pdo = new PDO('mysql:host=' . DB_HOST . ';charset=UTF8mb4', DB_USER, DB_PASS);
-
-const DB_NAME = 'RipDB';
-
-$in = readline("Deploying to " . DB_HOST . '. Is this OK? [Y or Enter to continue]');
-
-$in = strtoupper($in);
-if ($in == 'Y' || $in == '') {
-	if (!$pdo) {
-		echo "Database connection failed! Please check the connection details in this file (deploy.php).";
-		exit();
-	} else {
-		$pdo->exec('DROP DATABASE IF EXISTS ' . DB_NAME . ';');
-		$pdo->exec('CREATE DATABASE ' . DB_NAME . ' CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci;');
-		$pdo = new PDO('mysql:host=' . DB_HOST . ';dbname=' . DB_NAME . ';charset=UTF8', DB_USER, DB_PASS);
-	}
-
-	function deployFiles($pdo, string $folder, array $fileNames)
-	{
-		foreach ($fileNames as $file) {
-			$sql = file_get_contents("$folder/$file.sql");
-			echo "Deploying: $folder/$file.sql\n";
-			$pdo->exec($sql);
-		}
-	}
-
-	// ------
-	// TABLES
-	// ------
-
-	$files = [
-		'Metas',
-		'Tags',
-		'Channels',
-		'Jokes',
-		'Games',
-		'Rippers',
-		'MetaJokes',
-		'JokeMetas',
-		'JokeTags',
-		'Genres',
-		'Rips',
-		'RipJokes',
-		'RipGenres',
-		'RipRippers',
-		'RipGuesserGame',
-		'RipGuesserUpvotes',
-		'RipJokeFeedback',
-		'Accounts'
-	];
-	deployFiles($pdo, 'Tables', $files);
-
-	// ------
-	// VIEWS
-	// ------
-
-	$files = [
-		'vw_RipsDetailed',
-		'vw_JokesDetailed'
-	];
-	deployFiles($pdo, 'Views', $files);
-
-	// ------
-	// PROCEDURES
-	// ------
-
-	$files = [
-		'usp_InsertJoke_SAMPLE',
-		'usp_InsertMetaJoke_SAMPLE',
-		'usp_InsertMeta',
-		'usp_InsertJoke',
-		'usp_InsertMetaJoke',
-		'usp_InsertRip',
-		'usp_UpdateRip',
-		'usp_DeleteRip',
-		'usp_InsertTag',
-		'usp_InsertChannel',
-		'usp_InsertGame',
-		'usp_InsertRipper',
-		'usp_PurgeInactiveRipGuesserGames',
-		'usp_NewRipGuesserGame',
-		'usp_PingRipGuesserGame',
-		'usp_SelectRandomRip',
-		'usp_InsertRipFeedback',
-		'usp_InsertLogin',
-		'usp_SelectLogin'
-	];
-	deployFiles($pdo, 'Procedures', $files);
-
-	// ------
-	// Base Data
-	// ------
-
-	deployFiles($pdo, 'Scripts', ['BaseData']);
-
-	// ----------------------
-	// Sample Data (optional)
-	// ----------------------
-
-	$in = readline("Database deployment successful! Would you like to deploy some sample data? [y/N]");
+	$in = readline("Deploying to " . constant('SQL_HOST') . '. Is this OK? [Y or Enter to continue]');
 
 	$in = strtoupper($in);
-	if ($in == 'Y') {
-		deployFiles($pdo, 'Scripts', ['Sample_data']);
+	if ($in == 'Y' || $in == '') {
+		if (!$pdo) {
+			echo "Database connection failed! Please check the connection details in this file (deploy.php).";
+			exit();
+		} else {
+			$pdo->exec('DROP DATABASE IF EXISTS ' . constant('SQL_DB') . ';');
+			$pdo->exec('CREATE DATABASE ' . constant('SQL_DB') . ' CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci;');
+			$pdo = new PDO('mysql:host=' . constant('SQL_HOST') . ';dbname=' . constant('SQL_DB') . ';charset=UTF8', constant('SQL_USER'), constant('SQL_PASS'));
+		}
+
+		function deployFiles($pdo, string $folder, array $fileNames)
+		{
+			foreach ($fileNames as $file) {
+				$sql = file_get_contents("$folder/$file.sql");
+				echo "Deploying: $folder/$file.sql\n";
+				$pdo->exec($sql);
+			}
+		}
+
+		// ------
+		// TABLES
+		// ------
+
+		$files = [
+			'Metas',
+			'Tags',
+			'Channels',
+			'Jokes',
+			'Games',
+			'Rippers',
+			'MetaJokes',
+			'JokeMetas',
+			'JokeTags',
+			'Genres',
+			'Rips',
+			'RipJokes',
+			'RipGenres',
+			'RipRippers',
+			'RipGuesserGame',
+			'RipGuesserUpvotes',
+			'RipJokeFeedback',
+			'Accounts'
+		];
+		deployFiles($pdo, 'Tables', $files);
+
+		// ------
+		// VIEWS
+		// ------
+
+		$files = [
+			'vw_RipsDetailed',
+			'vw_JokesDetailed'
+		];
+		deployFiles($pdo, 'Views', $files);
+
+		// ------
+		// PROCEDURES
+		// ------
+
+		$files = [
+			'usp_InsertMeta',
+			'usp_InsertJoke',
+			'usp_InsertMetaJoke',
+			'usp_InsertRip',
+			'usp_UpdateRip',
+			'usp_DeleteRip',
+			'usp_InsertTag',
+			'usp_InsertChannel',
+			'usp_InsertGame',
+			'usp_UpdateGame',
+			'usp_InsertRipper',
+			'usp_PurgeInactiveRipGuesserGames',
+			'usp_NewRipGuesserGame',
+			'usp_PingRipGuesserGame',
+			'usp_SelectRandomRip',
+			'usp_InsertRipFeedback',
+			'usp_InsertLogin',
+			'usp_SelectLogin',
+			'usp_UpdateAccountPassword',
+			'usp_UpdateAccountUsername'
+		];
+
+		deployFiles($pdo, 'Procedures', $files);
+
+		// ------
+		// Base Data
+		// ------
+
+		deployFiles($pdo, 'Scripts', ['BaseData']);
+
+		// ----------------------
+		// Sample Data (optional)
+		// ----------------------
+
+		$in = readline("Database deployment successful! Would you like to deploy some sample data? [y/N]");
+
+		$in = strtoupper($in);
+		if ($in == 'Y') {
+			$sampleProcedures = [
+				'usp_InsertJoke_SAMPLE',
+				'usp_InsertMetaJoke_SAMPLE'
+			];
+			deployFiles($pdo, 'Procedures', $sampleProcedures);
+
+			deployFiles($pdo, 'Scripts', ['Sample_data']);
+
+			foreach ($sampleProcedures as $procName) {
+				echo "Dropping: $procName\n";
+				$pdo->exec("DROP PROCEDURE $procName;");
+			}
+		}
+	} else {
+		echo 'Aborting.';
 	}
-} else {
-	echo 'Aborting.';
 }
